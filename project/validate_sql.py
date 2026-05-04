fafe20270c15853ffc54e55d8d8c11dc869432db
Lokendar <lokendar7991@email.com>
2026-05-04 20:53:33 +0530

    sql ci push

---
+++ project/validate_sql.py
@@ -0,0 +1,23 @@
+import os
+
+SQL_DIR = "sql"
+
+def validate_file(filepath):
+    with open(filepath, "r") as f:
+        content = f.read().strip()
+
+        if not content:
+            raise Exception(f"{filepath} is empty ❌")
+
+        if not content.lower().startswith("select"):
+            raise Exception(f"{filepath} should start with SELECT ❌")
+
+def main():
+    for file in os.listdir(SQL_DIR):
+        if file.endswith(".sql"):
+            validate_file(os.path.join(SQL_DIR, file))
+
+    print("All SQL files are valid ✅")
+
+if __name__ == "__main__":
+    main()
